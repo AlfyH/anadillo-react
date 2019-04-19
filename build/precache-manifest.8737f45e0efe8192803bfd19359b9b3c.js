9d80c7177266c46a69d8c4e076428342e4e53dc0
Alfy Hushairi <alfyhushairi@gmail.com>
2019-04-19 15:56:24 +0800

    u

---
+++ build/precache-manifest.8737f45e0efe8192803bfd19359b9b3c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "30236b001758f5bb8c06297dbf750828",
+    "url": "/anadillo-react/static/media/logo.30236b00.png"
+  },
+  {
+    "revision": "fc8b4ca34164b652e2c031838d50bb44",
+    "url": "/anadillo-react/static/media/linkedin.fc8b4ca3.png"
+  },
+  {
+    "revision": "80743e2c37c9d1d95b7855260ca2eba0",
+    "url": "/anadillo-react/static/media/alfy.80743e2c.png"
+  },
+  {
+    "revision": "21f03e310f6d76616d796e0251158fee",
+    "url": "/anadillo-react/static/media/DancingScript-Regular.21f03e31.otf"
+  },
+  {
+    "revision": "f3800cbac04291ab5e18",
+    "url": "/anadillo-react/static/js/runtime~main.e212a9ba.js"
+  },
+  {
+    "revision": "23e548228fa6ea4f354a",
+    "url": "/anadillo-react/static/js/main.46b15f86.chunk.js"
+  },
+  {
+    "revision": "31053b4b0f23c02f09dc",
+    "url": "/anadillo-react/static/js/2.ce561293.chunk.js"
+  },
+  {
+    "revision": "23e548228fa6ea4f354a",
+    "url": "/anadillo-react/static/css/main.70bd462b.chunk.css"
+  },
+  {
+    "revision": "629c7f276a16fd51a87f4ce469e2caf2",
+    "url": "/anadillo-react/index.html"
+  }
+];
